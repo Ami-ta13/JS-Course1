7833eaa22accf34024ba79b12912b317937ff8a4
Amita_Biswal <139998495+Ami-ta13@users.noreply.github.com>
2025-05-16 14:30:34 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "amita"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" =>33
+// "33abc" =>NAN
+// true =>1;false =>0
+
+let isLoggedIn = "amita"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 =>true; 0 =>false
+// "" =>false
+// "amita" =>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log( stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
