4b48708ee371ebbd80ce139ce97108b0252cc6e0
Amita_Biswal <139998495+Ami-ta13@users.noreply.github.com>
2025-05-16 12:22:33 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12364
+let accountEmail = "amita@google.com"
+var accountPassword = "12689"
+accountCity="jaipur"
+// let accountState
+
+
+
+// accountId = 2 //not allowed
+accountEmail="amita1@google.com"
+accountPassword="3456"
+accountCity="Bengaluru"
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
