76f5f65f17fe30f2f343cea0cfc979e72f6db887
Amita_Biswal <139998495+Ami-ta13@users.noreply.github.com>
2025-05-22 09:44:11 +0000

    json talks

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -41,12 +41,38 @@ const tinderUser = {}
  ]
 
  users[1].email
- console.log(tinderUser);
+ //console.log(tinderUser);
 
- console.log(Object.keys(tinderUser));
- console.log(Object.values(tinderUser));
- console.log(Object.entries(tinderUser));
- console.log(tinderUser.hasOwnProperty('isLogged'));
+//  console.log(Object.keys(tinderUser));
+//  console.log(Object.values(tinderUser));
+//  console.log(Object.entries(tinderUser));
+//  console.log(tinderUser.hasOwnProperty('isLogged'));
+
+ const course ={
+    coursename: "javascript",
+    price: "999",
+    courseInstructor: "amita" 
+ }
+ //course.courseInstructor
+
+ const {courseInstructor: instructor} = course
+
+ //console.log(courseInstructor);
+ console.log(instructor);
+
+//  {
+//     "name": "Amita",
+//     "coursename": "javascript",
+//     "price": "free"
+//  }
+ 
+
+[
+    {},
+    {},
+    {}
+]
+ 
  
  
  
