7be2bdb43e4a6e1456880000c8bc6c1b6280f946
Amita_Biswal <139998495+Ami-ta13@users.noreply.github.com>
2025-05-24 14:43:25 +0000

    if

---
+++ 04_control_flow/one.js
@@ -0,0 +1,47 @@
+// if
+
+const isUserloggedIn = true
+const temperature = 41
+
+if(temperature === 41){
+  //console.log("less then 50");
+  
+}else{
+//console.log("temperature is greater than 50");
+}
+
+// console.log("execute");
+
+// <,>,<=,>=,==,!=,=== , !==
+
+// const score = 200
+// if(score > 100){
+//     const  power = "fly"
+//     console.log(`user power: ${power}`);
+    
+// }
+// console.log(`user power: ${power}`);
+
+//const balance = 1000
+
+//if(balance >500) console.log("test"),console.log("test2");// not readable,not a good practise
+
+// 
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard) {
+    console.log("Allow to buy course");
+    
+}
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("user logged in");
+    
+}
+
+
+
+
